64383b70a4a43af721f1a4da26bb542cb22cf9f1
Ashcroft-Haley <43455068+Ashcroft-Haley@users.noreply.github.com>
2018-12-18 20:27:44 -0800

    Update home-javascript.js

--- doc/final-app/home-javascript.js
+++ doc/final-app/home-javascript.js
@@ -1,4 +1,4 @@
 window.onload = function() {
     var title = document.getElementById('title')
-    title.style.boxShadow = "2px 4px 20px rgba(0, 0, 0, .30);"
+    title.style.boxShadow = "2px 4px 20px rgba(0, 0, 0, .80);"
 }
